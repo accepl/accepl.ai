fef22d125431ac37d7798529ff1d1755da409a2f
accepl <adityasingh@accepl.com>
2025-02-23 00:11:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-import uvicorn
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from api import app
-
-# ✅ Serve static files (HTML, CSS, JS)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=10000)
